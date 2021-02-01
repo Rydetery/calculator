2b29aa74532d26bd2f847c144df7a8d6e008502e
Rydetery <72303215+Rydetery@users.noreply.github.com>
2021-02-01 20:42:39 +0300

    Calculator
    Online calculator based on JS

---
+++ script.js
@@ -0,0 +1,17 @@
+function insert(num){
+ document.form.textview.value = document.form.textview.value + num;
+}
+function clean() {
+    document.form.textview.value = '';
+}
+function arrow() {
+    let exp = document.form.textview.value;
+    document.form.textview.value = exp.substring(0, exp.length -1);
+}
+function calculate(){
+    let exp = document.form.textview.value;
+    if (exp) {
+    exp = exp.replace(/%/g, "/100");
+    document.form.textview.value = eval(exp);
+    }
+}
